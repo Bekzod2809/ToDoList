6727fe9244da3779cd013d2bfc9075cb79d62154
Bekzod <bekzodjonxasanov7@gmail.com>
2026-06-02 16:39:59 +0500

    WI-005 Added

---
+++ ToDoList/src/ToDoListPro/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListPro.Entities;
+
+namespace ToDoListPro.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Todo> Todos { get; set; }
+    public DbSet<User> Users { get; set; }
+
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
